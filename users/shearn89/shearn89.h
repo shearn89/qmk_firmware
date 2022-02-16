112bbf2a210e517908fcc30c80b3473bf36ec170
Alex Shearn <ajshearn@amazon.co.uk>
2022-02-16 16:13:06 +0000

    adding dz60 stuff

--- users/shearn89/shearn89.h
+++ users/shearn89/shearn89.h
@@ -4,23 +4,17 @@
 #pragma once
 
 #include "quantum.h"
-#include "annepro2.h"
 
-enum ap_keycodes {
-  QWERTY = AP2_SAFE_RANGE,
+enum user_keycodes {
+  QWERTY = SAFE_RANGE,
   WORKMAN,
 };
 
-enum anne_pro_layers {
+enum user_layers {
   _WORKMAN,
   _QWERTY,
   _FN1_LAYER,
   _FN2_LAYER,
 };
-
-// turn off leds when pc sleeps
-#define RGBLIGHT_SLEEP
-// turn on leds
-#define RGBLIGHT_ENABLE
 
 #endif
