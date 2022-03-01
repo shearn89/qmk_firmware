80c2b1b5179e47ca24fd4e882c3025c3951b571d
Alex Shearn <ajshearn@amazon.co.uk>
2022-03-01 14:58:02 +0000

    trying to get colours on layers to work

--- users/shearn89/shearn89.c
+++ users/shearn89/shearn89.c
@@ -24,3 +24,30 @@ __attribute__((weak)) void keyboard_post_init_user(void) {}
 __attribute__((weak)) void suspend_power_down_user(void) {}
 __attribute__((weak)) void matrix_init_user(void) { }
 __attribute__((weak)) void matrix_scan_user(void) { }
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    switch(biton32(state)) {
+    case _QWERTY:
+        // rgb_matrix_mode(RGB_MATRIX_HUE_WAVE);
+        rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
+        rgb_matrix_set_color_all(RGB_GOLDENROD);
+        break;
+    case _WORKMAN:
+        rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
+        rgb_matrix_set_color_all(RGB_GOLD);
+        break;
+    case _FN1_LAYER:
+        rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
+        rgb_matrix_set_color_all(RGB_GREEN);
+        break;
+    case _FN2_LAYER:
+        rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
+        rgb_matrix_set_color_all(RGB_CYAN);
+        break;
+    default: //  for any other layers, or the default layer
+        rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
+        rgb_matrix_set_color_all(RGB_WHITE);
+        break;
+    }
+  return state;
+}
