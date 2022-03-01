dd80d2f3a29076b6068680fe56e6ad525b108539
Alex Shearn <ajshearn@amazon.co.uk>
2022-03-01 12:16:27 +0000

    adding flash script for dz60 with lufa-ms bootloader

---
+++ flash-dz60v21.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+qmk compile -kb dztech/dz60rgb_ansi/v2_1 -km shearn89
+echo "reconnect in bootloader mode, will continue in 30 seconds..."
+sleep 10
+echo "20 seconds..."
+sleep 10
+echo "10 seconds..."
+sleep 10
+echo "continuing"
+rm /Volumes/KBDFANS\ \ /FLASH.BIN
+cp dztech_dz60rgb_ansi_v2_1_shearn89.bin /Volumes/KBDFANS\ \ /FLASH.bin
+echo "complete"
