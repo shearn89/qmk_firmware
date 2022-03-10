57876a75f1ed5b3f724ddfbf5f1f9bea31df371d
Alex Shearn <ajshearn@amazon.co.uk>
2022-03-10 17:33:06 +0000

    playing with lighting, still

--- users/shearn89/shearn89.c
+++ users/shearn89/shearn89.c
@@ -29,25 +29,65 @@ HSV hsv;
 
 __attribute__((weak)) void keyboard_post_init_user(void) {
   rgb_matrix_mode(RGB_MATRIX_HUE_WAVE);
+  // rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
   hsv = rgb_matrix_get_hsv();
 }
 
 // matrix effects are done with HSV, so set HSV colors
 __attribute__((weak)) layer_state_t layer_state_set_user(layer_state_t layer) {
     switch(get_highest_layer(layer)) {
+    case _QWERTY:
+        rgb_matrix_sethsv_noeeprom(142, 255, 255); //bluish
+        rgb_matrix_mode_noeeprom(RGB_MATRIX_HUE_WAVE);
+        break;
+    case _WORKMAN:
+        rgb_matrix_sethsv_noeeprom(hsv.h, hsv.s, hsv.v); // red?
+        rgb_matrix_mode_noeeprom(RGB_MATRIX_HUE_WAVE);
+        break;
     case _FN1_LAYER:
-        rgb_matrix_sethsv_noeeprom(HSV_CYAN);
+        rgb_matrix_sethsv_noeeprom(HSV_WHITE);
+        rgb_matrix_mode_noeeprom(RGB_MATRIX_HUE_WAVE);
         break;
     case _FN2_LAYER:
-        rgb_matrix_sethsv_noeeprom(HSV_PURPLE);
-        break;
-    default:
-        rgb_matrix_sethsv_noeeprom(hsv.h, hsv.s, hsv.v);
+        rgb_matrix_sethsv_noeeprom(HSV_GREEN);
+        rgb_matrix_mode_noeeprom(RGB_MATRIX_HUE_WAVE);
         break;
     }
     return layer;
 }
 
+// LED_FLAG_MODIFIER = mods
+// LED_FLAG_KEYLIGHT = alphas
+
+/*
 // use this to do clever stuff with indicators or specific keys.
 // https://github.com/qmk/qmk_firmware/blob/master/docs/feature_rgb_matrix.md#indicators
-__attribute__((weak)) void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) { }
+__attribute__((weak)) void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    if (get_highest_layer(layer_state) > _WORKMAN) {
+        // disable effect?
+        // rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
+        // rgb_matrix_sethsv_noeeprom(HSV_OFF);
+
+        uint8_t layer = get_highest_layer(layer_state);
+
+        for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
+            for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
+                uint8_t index = g_led_config.matrix_co[row][col];
+
+                if (index >= led_min && index <= led_max &&
+                keymap_key_to_keycode(layer, (keypos_t){col,row}) > KC_TRNS) {
+                    rgb_matrix_set_color(index, RGB_CYAN);
+                }
+            }
+        }
+    }
+}
+*/
+
+__attribute__((weak)) void suspend_power_down_kb(void) {
+    rgb_matrix_set_suspend_state(true);
+}
+
+__attribute__((weak)) void suspend_wakeup_init_kb(void) {
+    rgb_matrix_set_suspend_state(false);
+}
