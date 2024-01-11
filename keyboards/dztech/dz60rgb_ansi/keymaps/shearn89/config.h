6ef51273ea5ffe82e3af0d92dffac2088f3bd82e
shearn89 <alexander.shearn@gmail.com>
2024-01-11 16:57:58 +0000

    adding missing files

---
+++ keyboards/dztech/dz60rgb_ansi/keymaps/shearn89/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+#define AUTO_SHIFT_TIMEOUT 120
+#define NO_AUTO_SHIFT_SPECIAL
+
---
+++ keyboards/dztech/dz60rgb_ansi/keymaps/shearn89/rules.mk
@@ -0,0 +1,5 @@
+COMMAND_ENABLE = no
+# AUTOCORRECT_ENABLE = yes ## cool feature
+# KEY_LOCK_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+AUTO_SHIFT_ENABLE = yes
